fd7223439383fdcfb4bb94efda934d3e470abcf0
Chris Reynolds <chris@jazzsequence.com>
2025-12-05 10:29:08 -0700

    add an update WP workflow

---
+++ .github/workflows/bin/check-wp-version.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -e
+
+get_current_wp_version() {
+	curl -s https://api.wordpress.org/core/version-check/1.7/ | jq -r '.offers[0].current'
+}
+
+get_site_wp_version() {
+	terminus wp ${SITE_ID}.live -- core version
+}
+
+compare_versions() {
+	local current_version
+	local site_version
+
+	current_version=$(get_current_wp_version)
+	site_version=$(get_site_wp_version)
+
+	if php -r "exit(version_compare('$site_version', '$current_version', '<') ? 0 : 1);"; then
+		echo "The site is running an outdated version of WordPress: $site_version (current: $current_version)"
+		update_wordpress
+	else
+		echo "The site is running the current version of WordPress: $site_version"
+	fi
+}
+
+update_wordpress() {
+	cd ${WP_PATH}
+	git checkout -b update-wp-version-$(get_current_wp_version)-$(date +%Y%m%d)
+
+	wp core update
+
+	local wp_version
+	wp_version=$(wp core version)
+	echo "Updating WordPress to version ${wp_version} in ${WP_PATH}"
+	git add .
+	git commit -m "Update WordPress to version ${wp_version}"
+}
+
+compare_versions
